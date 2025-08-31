3a64a82295373a388f21754b9d65ffd19e9d502e
Lahiru Amarasooriya <lahirudx@gmail.com>
2025-08-31 23:11:18 +0530

    Initial Commit

---
+++ SwiftfulMapApp/SwiftfulMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulMapAppApp.swift
+//  SwiftfulMapApp
+//
+//  Created by Lahiru Amarasooriya on 2025-08-31.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
