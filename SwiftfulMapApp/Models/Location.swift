be5d60361629520980fbe13856756a57970db578
Lahiru Amarasooriya <lahirudx@gmail.com>
2025-08-31 23:36:39 +0530

    Project Setup with Assets and Custom Data Model

---
+++ SwiftfulMapApp/Models/Location.swift
@@ -0,0 +1,19 @@
+//
+//  Location.swift
+//  SwiftfulMapApp
+//
+//  Created by Lahiru Amarasooriya on 2025-08-31.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
---
+++ SwiftfulMapApp/Views/LocationsView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationsView.swift
+//  SwiftfulMapApp
+//
+//  Created by Lahiru Amarasooriya on 2025-08-31.
+//
+
+import SwiftUI
+
+struct LocationsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationsView()
+}
